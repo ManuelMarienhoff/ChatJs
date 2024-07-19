b5a7d0293b7d7b306a89f8b46cddfd374c9800fa
Manuel Marienhoff <manuelmarienhoff1@gmail.com>
2024-07-19 14:01:03 -0300

    Add CRUD operations for users during login and logout

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,55 @@
-// conexion de socket del cliente
+// conexion socket del cliente
 
 $(function(){
     const socket = io(); /* mantiene conexion en tiempo real con el sevidor */
 
     /* obtaining DOM elemens from the interface */
-    const messageForm = $('#message-form');
-    const message = $('#message');
-    const chat = $('#chat');
+    const $messageForm = $('#message-form');
+    const $message = $('#message');
+    const $chat = $('#chat');
+
+    /* obtaining DOM elemens from the nickname form */
+    const $nickForm = $('#nick-form');
+    const $nickname = $('#nickname');
+    const $nickError = $('#nick-error');
+    const $usernames = $('#usernames');
+
+    $nickForm.submit(e=>{
+        e.preventDefault();
+        socket.emit('new user', $nickname.val(), function(dataCallback){ /* envia al servidor nickname y callback */
+            if(dataCallback){ /* si existe el usuario, ocultamos interfaz nickname y mostramos chat */
+                $('#nick-wrap').hide();
+                $('#content-wrap').show();
+            } else {
+                $nickError.html(`
+                    <div class='alert alert-danger'>
+                    Username already taken
+                    </div>`);
+            }
+            $nickname.val('');
+        });
+    });
+
+
 
     /* events */
-    messageForm.submit(e=>{
+    $messageForm.submit(e=>{
         e.preventDefault();
-        socket.emit('send message', message.val());  /* envio datos desde el cliente al servidor como 'send message' */
-        message.val('')
+        socket.emit('send message', $message.val());  /* envio datos desde el cliente al servidor como 'send message' */
+        $message.val('')
     })
 
     /* Escucho el mensaje que viene desde el servidor */
-    socket.on('new message', function(data){
-        chat.append(data + '<br/>')
+    socket.on('new message', function(message){
+        $chat.append(message + '<br/>')
     } )
 
+    /* Recibo todos los usuarios activos del servidor */
+    socket.on('usernames', nicknames =>{
+        let html = '';
+        for (let i=0; i < nicknames.length; i++){
+            html += `<p><i class='fa fa-user'></i> ${nicknames[i]}</p>`
+        }
+        $usernames.html(html)
+    })
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,15 +1,41 @@
-// conexion de socket del servidor
+// conexion socket del servidor
 
 module.exports = function(io){
+
+    let nicknames = [];
+
 
     io.on("connection", (socket) => { 
         console.log("new user connected");
 
+        /* recibe nickname y callback */
+        socket.on('new user', (nickname ,callback)=>{
+            if (nicknames.indexOf(nickname) >= 0){ /* si existe, no le muestro la interfaz del chat */
+                callback(false) ;
+            } else { /* si no existe, lo agregamos */
+                callback(true);
+                socket.nickname = nickname; /* guardamos el nickname en la websocket */
+                nicknames.push(socket.nickname);
+                updateNicknames() /* envio todos los usuarios activos al cliente */
+            }
+        }); 
+
         /* el servidor escucha el mensaje que se manda desde el cliente y lo reenvia a todos los clientes */
         socket.on('send message', function(data){  /* escucho mensaje del cliente */
             io.sockets.emit('new message', data) /* envio datos a todos los sockets */
-        })
+        });
         
+        socket.on('disconnect', data => {
+            if(!socket.nickname) return; /* si no encuentra usuario, return */
+            nicknames.splice(nicknames.indexOf(socket.nickname), 1); /* si encuentra, lo elimina del server*/
+            updateNicknames() /* envio todos los usuarios activos al cliente */
+
+        })
+
+        function updateNicknames(){
+            io.sockets.emit('usernames', nicknames) /* envio todos los usuarios activos al cliente */
+
+        }
 
     });
 }
